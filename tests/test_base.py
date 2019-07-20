5ff0b5a5ed40076e2dff173f515a044d497e58c5
JavierLuna <javierlunamolina@gmail.com>
2019-07-20 14:00:50 +0200

    Fix datastore emulator not working with mocked credentials. Thanks google.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,37 +1,10 @@
 import unittest
 
-import requests
-from google.auth.credentials import Credentials
-
 from datastorm import DataStorm
-
-
-class EmulatorCreds(Credentials):
-    """A mock credential object.
-    Used to avoid unnecessary token refreshing or reliance on the network
-    while an emulator is running.
-    """
-
-    def __init__(self):  # pylint: disable=super-init-not-called
-        self.token = b'seekrit'
-        self.expiry = None
-
-    @property
-    def valid(self):
-        """Would-be validity check of the credentials.
-        Always is :data:`True`.
-        """
-        return True
-
-    def refresh(self, unused_request):  # pylint: disable=unused-argument
-        """Off-limits implementation for abstract method."""
-        raise RuntimeError('Should never be refreshed.')
 
 
 class TestBase(unittest.TestCase):
     TEST_PROJECT_ID = "datastorm-test"
-    DUMMY_CREDENTIALS = EmulatorCreds
-    _HTTP = requests.Session
 
     def setUp(self):
         from datastorm import fields
