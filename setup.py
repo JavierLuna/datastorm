ffe84b6342a2f50eaffda992e5ce0965abcb98c8
JavierLuna <javierlunamolina@gmail.com>
2019-10-06 20:37:17 +0200

    Bump version to 0.0.0a6

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+import datastorm
 
 setup(
     name='datastorm',
-    version='0.0.0a5',
+    version=datastorm.__version__,
     description='Simple and easy to use ODM for Google Datastore. Documentation: https://datastorm-docs.rtfd.io',
     url='https://github.com/JavierLuna/datastorm',
     author='Javier Luna Molina',
